f40fc0b50839387dd4df45e959bee6a37d0241bc
Mist <59188378+Mist0090@users.noreply.github.com>
2022-07-24 02:45:20 +0900

    Add files via upload

---
+++ 日本語版MEMZ/VC/MEMZ/data.h
@@ -0,0 +1,22 @@
+#pragma once
+
+CONST unsigned char code1[];
+CONST unsigned char code2[];
+CONST unsigned char code[];
+
+CONST size_t code_len;
+CONST size_t code1_len;
+CONST size_t code2_len;
+
+CONST unsigned char msg[];
+CONST size_t msg_len;
+
+CONST char* sites[];
+CONST size_t nSites;
+
+const size_t nSounds;
+
+CONST char* sounds[];
+
+const size_t nMsgs;
+const char* msgs[];
